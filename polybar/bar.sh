059eade76798f5d86fce9ccd23efdfec151ba21b
Matt Dobson <dobbie03@icloud.com>
2021-01-09 17:22:06 +1300

    Jan 2021 Update

--- polybar/bar.sh
+++ polybar/bar.sh
@@ -4,8 +4,8 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 5; done
 
 # Launch bar1 and bar2
-polybar top -r &
+sleep 6; polybar top -r &
 
